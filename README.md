ff9faa2d97b5de453b9f335088a4bb8d6b360689
falmada21 <fernando.almada@gmail.com>
2021-04-02 20:40:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wrqtzq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wrqtzq)
---
+++ index.js
@@ -0,0 +1,30 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let numbers = [4,8,3,6,2];
+let result = numbers[1]*3;
+
+console.log(result);
+
+
+let tvshows =["Friends","Money Heist","The Office","Ozark"];
+let thirdshow = "Imposters";
+tvshows[2] = thirdshow;
+console.log(tvshows)
+
+
+let myStudies =["Math","Physics","Programming","Sociology"]
+myStudies.pop();
+console.log(myStudies);
+
+let food1 = "Tacos"
+let food2 = "Burritos"
+let food3 = "Hamburgers"
+let food4 = "Pizza"
+let food5 = "Sushi"
+
+let myFavoriteFood[] = [food1,food2,food3,food4,food5];
